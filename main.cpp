bbdb0eb98298810f9fbe7f23f7443a6b69c9cb74
Kuba P - WSL <jpiskozub@gmail.com>
2024-02-13 21:46:08 +0100

    Basic SQL SELECT added

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ using namespace pqxx;
    std::string connstring = "dbname = "+ dbName + "user = "+ username +" password = "+ pswrd +" hostname = "+ hostname + " port = "+ port;
 
    try {
-       pqxx::connection c("dbname = handpi  user = PC password = raspberrydbpi \
+      connection c("dbname = handpi  user = PC password = raspberrydbpi \
       hostaddr = 192.168.1.100 port = 5432");
        std::cout << "Connected to " << c.dbname() << '\n';
        if (c.is_open()) {
@@ -40,11 +40,30 @@ using namespace pqxx;
          cout << "Can't open database" << endl;
          return 1;
         }
-    
-   }
+        std:: string sql = "SELECT * from patient_data";
+
+      nontransaction N(c);
+      
+      result R( N.exec( sql ));
+      
+      for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
+         cout << "ID = " << c[0].as<int>() << endl;
+         cout << "exam_ID = " << c[1].as<int>() << endl;
+         cout << "Gender = " << c[2].as<string>() << endl;
+         cout << "Age = " << c[3].as<int>() << endl;
+         cout << "mcsd = " << c[4].as<string>() << endl;
+         cout << "Palm_size = " << c[5].as<float>() << endl;
+
+      }
+      cout << "Operation done successfully" << endl;
+      //c.disconnect ();
+
+    }
     catch (const std::exception &e) {
       cerr << e.what() << std::endl;
       return 1;
 
     }
-}
+
+
+   } 
